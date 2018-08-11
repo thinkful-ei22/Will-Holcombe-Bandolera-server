992718cd57078d4a6eb435ddbd7773cea9bd6b5e
William Holcombe <william.holcombe@uconn.edu>
2018-08-11 10:48:11 -0400

    space

--- passport/jwt.js
+++ passport/jwt.js
@@ -3,13 +3,13 @@ const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const { JWT_SECRET } = require('../config');
 
 const options = {
-    secretOrKey: JWT_SECRET,
-    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
-    algorithms: ['HS256']
-  };
+  secretOrKey: JWT_SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+  algorithms: ['HS256']
+};
   
-  const jwtStrategy = new JwtStrategy(options, (payload, done) => {
-    done(null, payload.user);
-  });
+const jwtStrategy = new JwtStrategy(options, (payload, done) => {
+  done(null, payload.user);
+});
 
-  module.exports = jwtStrategy;
+module.exports  = jwtStrategy;
